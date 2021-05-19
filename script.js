f271df522f27dbdbd77cfd32018e5b28538725bb
manideep-8 <60574685+manideep-8@users.noreply.github.com>
2021-05-19 15:18:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function validate() {
+
+  var a= document.getElementById('pwd1').value;
+  var b= document.getElementById('pwd2').value;
+
+
+    if (a!==b)
+     {
+       alert("it is wrong")
+    }
+
+}
